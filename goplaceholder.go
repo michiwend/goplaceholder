c1c7a05eb672e3d83da0b37b2e7be7c991f7152d
Michael Wendland <accounts@michiwend.com>
2014-10-15 00:45:23 +0200

    license header, doc

--- goplaceholder.go
+++ goplaceholder.go
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2014 Michael Wendland
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *	Authors:
+ *		Michael Wendland <michael@michiwend.com>
+ */
+
+/*
+package goplaceholder implements a simple library to generate placeholder
+images using freetype-go.
+*/
 package goplaceholder
 
 import (
@@ -20,6 +49,8 @@ const (
 	testFontSize               = 1.00 // use heigher values (>=100) when hinting enabled
 )
 
+// Placeholder returns a placeholder image with the given text or, if text was
+// an empty string, with the image bounds in the form "800x600".
 func Placeholder(text, ttfPath string, foreground, background color.RGBA, width, height int) (image.Image, error) {
 
 	if width < 0 || height < 0 {
