ff00b46ee84c46d91c8638afc4c102e3c462e1d9
Michael Wendland <accounts@michiwend.com>
2014-10-17 02:47:28 +0200

    adds simple http placeholder server

---
+++ service/placeholder.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"image/color"
+	"image/png"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/michiwend/goplaceholder"
+)
+
+func serveImage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "image/png")
+
+	params := mux.Vars(r)
+	r.ParseForm()
+	text := r.FormValue("text")
+	width, _ := strconv.ParseInt(params["width"], 10, 32)
+	height, _ := strconv.ParseInt(params["height"], 10, 32)
+	// FIXME err handling
+
+	img, _ := goplaceholder.Placeholder(
+		text,
+		"/usr/share/fonts/TTF/DejaVuSans-Bold.ttf",
+		color.RGBA{150, 150, 150, 255},
+		color.RGBA{204, 204, 204, 255},
+		int(width), int(height))
+
+	png.Encode(w, img)
+	log.Printf("served image: w: %d h: %d\n", width, height)
+}
+
+func main() {
+
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/placeholder/{width:[0-9]+}x{height:[0-9]+}.png", serveImage).Methods("GET")
+	rtr.HandleFunc("/placeholder/{width:[0-9]+}.png", serveImage).Methods("GET")
+
+	http.Handle("/", rtr)
+
+	log.Println("Listening...")
+	log.Fatal(http.ListenAndServe(":3000", nil))
+
+}
