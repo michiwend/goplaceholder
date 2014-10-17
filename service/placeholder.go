1318d0321bb4c454de5dbb21999a831ebdd1bf54
Michael Wendland <accounts@michiwend.com>
2014-10-17 16:28:45 +0200

    err handling

--- service/placeholder.go
+++ service/placeholder.go
@@ -20,18 +20,23 @@ func serveImage(w http.ResponseWriter, r *http.Request) {
 	height, _ := strconv.ParseInt(params["height"], 10, 32)
 	// FIXME err handling
 
-	img, _ := goplaceholder.Placeholder(
 	if width > 4000 || height > 4000 {
 		w.Header().Set("Content-Type", "text/html")
 		w.Write([]byte("image too large"))
 		return
 	}
 
+	img, err := goplaceholder.Placeholder(
 		text,
 		"/usr/share/fonts/TTF/DejaVuSans-Bold.ttf",
 		color.RGBA{150, 150, 150, 255},
 		color.RGBA{204, 204, 204, 255},
 		int(width), int(height))
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
 
 	w.Header().Set("Content-Type", "image/png")
 	png.Encode(w, img)
